fbc3ec5d937d60622adc9f2019977403ab2bbc34
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2020-03-07 08:42:01 +0330

    vs code debugger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ export class AppController {
 
     @Get()
     getHello(): string {
+        this.appService.loadSchema();
         return this.appService.getHello();
     }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs-extra';
 
 @Injectable()
 export class AppService {
+    async loadSchema() {
+        const schema = await fs.readFile('./test/schema.graphql', { encoding: 'utf8' });
+        console.log(schema);
+    }
     getHello(): string {
         return 'Hello World!';
     }
