fb1269e34874451e052defb931c34b5fe80183f7
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2020-03-07 12:00:14 +0330

    basic nexus

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
-const watchMe = 'ne ne';
+import { queryType, stringArg, makeSchema } from 'nexus';
+import { GraphQLServer } from 'graphql-yoga';
 
-const sayHello = (name: string) => console.log(`Hello ${name} in node in vscode!`);
+const Query = queryType({
+    definition(t) {
+        t.string('hello', {
+            args: { name: stringArg({ nullable: true }) },
+            resolve: (parent, { name }) => `Hello ${name || 'World'}!`,
+        });
+    },
+});
 
-sayHello('typescript');
-debugger;
+const schema = makeSchema({
+    types: [Query],
+    outputs: {
+        // schema: __dirname + '/generated/schema.graphql',
+        // typegen: __dirname + '/generated/typings.ts',
+    },
+});
+
+const server = new GraphQLServer({
+    schema,
+});
+
+server.start(
+    {
+        port: process.env.PORT || 3000,
+    },
+    ({ port }) => console.log(`Server is running on http://localhost:${port}`),
+);
