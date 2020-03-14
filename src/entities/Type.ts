e418f5b2a440ec2bb9da3d3f514b4b3542da5f63
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2020-03-14 18:32:18 +0330

    typo

--- src/entities/Type.ts
+++ src/entities/Type.ts
@@ -9,10 +9,10 @@ export class Type extends Entity {
         super(definition.name, {
             name: definition.name,
             description: definition.description,
-            definition: (t) => this.typeDef.forEach(({ name, config }) => t.field(name, config)),
+            definition: (t) => this.fieldsTypeDef.forEach(({ name, config }) => t.field(name, config)),
         });
     }
-    private get typeDef() {
+    private get fieldsTypeDef() {
         return this.definition.fields.map((field) => {
             const type = Type.dictionary.get(field.type) as Model;
             return {
@@ -23,53 +23,5 @@ export class Type extends Entity {
                 } as FieldOutConfig<any, any>,
             };
         });
-    }
-    // protected graphQLSchema2(t: ObjectDefinitionBlock<string>) {
-    //     this.definition.fields.forEach((field) => {
-    //         let column = {
-    //             nullable: field.config.nullable,
-    //         } as EntitySchemaColumnOptions;
-    //         switch (field.type) {
-    //             case 'ID':
-    //                 column.primary = true;
-    //                 column.generated = true;
-    //                 column.type = 'int';
-    //                 t.id(field.name, field.config);
-    //                 break;
-    //             case 'String':
-    //             case 'File':
-    //             case 'DateTime':
-    //             case 'Email':
-    //             case 'Password':
-    //             case 'PhoneNumber':
-    //                 column.type = 'varchar';
-    //                 t.string(field.name, field.config);
-    //                 break;
-    //             case 'Boolean':
-    //                 column.type = 'boolean';
-    //                 t.boolean(field.name, field.config);
-    //                 break;
-    //             default:
-    //                 const ref = Type.dictionary.get(field.type) as Model;
-    //                 if (ref.isModel) {
-    //                     column = undefined;
-    //                     this.schema.relations[ref.name.toLowerCase()] = {
-    //                         name: ref.name.toLowerCase(),
-    //                         target: ref.name,
-    //                         nullable: field.config.nullable,
-    //                         type: 'one-to-one',
-    //                         cascade: true,
-    //                     } as EntitySchemaRelationOptions;
-    //                 } else {
-    //                     // it's an enum type or not model type
-    //                     column.type = 'varchar';
-    //                 }
-    //                 if (ref) t.field(field.name, { ...field.config, type: ref });
-    //                 break;
-    //         }
-    //         if (column) {
-    //             this.schema.columns[field.name] = column;
-    //         }
-    //     });
     }
 }
