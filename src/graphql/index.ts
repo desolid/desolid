b4132428437a2d5dd81873d7dd8e968b64e5e040
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2020-03-25 08:20:40 +0430

    GraphQLAPI

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -2,5 +2,5 @@ export * from './Type';
 export * from './Model';
 export * from './TypeDefinition';
 export * from './Schema';
-export * from './API';
+export * from './GraphQLAPI';
 export * from './scalars';
