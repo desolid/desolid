59f7de7d93bb246b27f6c27689911329a55476af
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2020-05-14 13:30:44 +0430

    fix: dist

--- src/cli.ts
+++ src/cli.ts
@@ -4,12 +4,12 @@ import * as clear from 'clear';
 import * as figlet from 'figlet';
 import * as path from 'path';
 import * as program from 'commander';
-import { version } from '../package.json';
+// import { version } from '../package.json';
 
 clear();
 console.log(chalk.green(figlet.textSync('Desolid', { horizontalLayout: 'full' })));
 program
-    .version(version)
+    .version(require('./package.json').version)
     .description(`Desolid: Single file BaaS\nhttps://github.com/desolid/desolid`)
     .option('-p, --path', 'Root desolid directory')
     .parse(process.argv);
