ae70e3ea2e2f15b0f8c0f01a5bdbf3b09dd2eddd
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2020-05-14 13:46:44 +0430

    feat: running desolid on cli

--- src/cli.ts
+++ src/cli.ts
@@ -4,12 +4,20 @@ import * as clear from 'clear';
 import * as figlet from 'figlet';
 import * as path from 'path';
 import * as program from 'commander';
-// import { version } from '../package.json';
+import Desolid from './Desolid';
 
-clear();
-console.log(chalk.green(figlet.textSync('Desolid', { horizontalLayout: 'full' })));
-program
-    .version(process.env.npm_package_version)
-    .description(`Desolid: Single file BaaS`)
-    .option('-p, --path', 'Root desolid directory')
-    .parse(process.argv);
+async function main() {
+    clear();
+    console.log(chalk.green(figlet.textSync('Desolid', { horizontalLayout: 'full' })));
+
+    // program
+    //     .version(process.env.npm_package_version)
+    //     .description(process.env.npm_package_description)
+    //     .option('-p, --path', 'Root desolid directory')
+    //     .parse(process.argv);
+
+    const desolid = new Desolid(process.cwd());
+    await desolid.start();
+}
+
+main();
